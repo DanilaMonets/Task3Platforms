42b9a72ba3c57b8f610fc1378e87cc2be4185db2
DanilMonets <danil.monets@gmail.com>
2018-12-10 15:23:49 +0200

    Added tests

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLogicLayer;
+using DataAccessLayer.OrderModel;
+using DataAccessLayer.SushiModels;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAddMethod()
+        {
+            OrdersList ol = new OrdersList();
+            ol.AddOrder(new Order());
+            Assert.AreEqual(ol.orders.Count, 1);
+        }
+        [TestMethod]
+        public void TestDeleteMethod()
+        {
+            OrdersList ol = new OrdersList();
+            Order o = new Order();
+            ol.AddOrder(o);
+            ol.DeleteOrder(o);
+            Assert.AreEqual(ol.orders.Count, 0);
+        }
+    }
+}
